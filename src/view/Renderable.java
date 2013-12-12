6b3c6b4b4c6fec2bbfc0b78ee07c4fbe945fad90
atklobas <atklobas@gmail.com>
2013-12-12 15:20:14 -0800

    test

--- src/view/Renderable.java
+++ src/view/Renderable.java
@@ -1,10 +1,12 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Image;
 
 public interface Renderable {
 	int getImageX();
 	int getImageY();
 	Image getImage();
+	public Color getColor();
 
 }
